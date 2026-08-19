b4dfed0412e5eb7edc3e24fe33b174e412346754
abimaeltvrs <abimaeltablet@gmail.com>
2026-08-18 22:56:53 -0300

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.pandasfc.app',
-  appName: 'PANDAS FC',
-  webDir: 'dist',
-};
-
-export default config;
